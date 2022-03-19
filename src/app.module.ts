96294483b89bf11cf77c787248ee1e5b45c93fe3
sjdkln <sajadkalantari@outlook.com>
2022-03-19 17:18:44 +0330

    fix config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CacheController } from './Controllers/cache.controller';
 import { CacheService } from './Services/cache.service';
 import * as config from 'config';
-import { CacheSchema } from './Models/cache.schema';
+import { Cache, CacheSchema } from './Models/cache.schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(config.get('db')),
+    MongooseModule.forRoot(config.get("db")),
     MongooseModule.forFeature([{ name: Cache.name, schema: CacheSchema }])
   ],
   controllers: [CacheController],
