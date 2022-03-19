84989cd54b2d83de83fde10f4a9b1525747bcc50
sjdkln <sajadkalantari@outlook.com>
2022-03-19 17:27:35 +0330

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Cache api node.js')
+    .setDescription('The cache api fro simulating cache functionality')
+    .setVersion('1.0')
+    .addTag('caches')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(config.get('port'));
 }
 bootstrap();
