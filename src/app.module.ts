43fbfae31abc2962d72f0e536342d15d8d09c04f
sjdkln <sajadkalantari@outlook.com>
2022-03-19 15:33:12 +0330

    add configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CacheController } from './Controllers/cache.controller';
 import { CacheService } from './Services/cache.service';
+import { config } from 'config'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/demo')],
+  imports: [MongooseModule.forRoot(config.get('db'))],
   controllers: [CacheController],
   providers: [CacheService],
 })
-export class AppModule {}
+export class AppModule { }
